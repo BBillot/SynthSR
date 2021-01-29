3ecb78e24c6a151a3bafcb94d038333929859c81
BBillot <benjamin.billot1993@gmail.com>
2021-01-29 17:37:02 +0000

    propagated changes in get_list_labels

--- SynthSR/brain_generator.py
+++ SynthSR/brain_generator.py
@@ -179,7 +179,7 @@ class BrainGenerator:
         if generation_labels is not None:
             self.generation_labels = utils.load_array_if_path(generation_labels)
         else:
-            self.generation_labels = utils.get_list_labels(labels_dir=labels_dir)
+            self.generation_labels, _ = utils.get_list_labels(labels_dir=labels_dir)
         if n_neutral_labels is not None:
             self.n_neutral_labels = n_neutral_labels
         else:
--- ext/lab2im/image_generator.py
+++ ext/lab2im/image_generator.py
@@ -108,7 +108,7 @@ class ImageGenerator:
         if generation_labels is not None:
             self.generation_labels = utils.load_array_if_path(generation_labels)
         else:
-            self.generation_labels = utils.get_list_labels(labels_dir=labels_dir)
+            self.generation_labels, _ = utils.get_list_labels(labels_dir=labels_dir)
         if output_labels is not None:
             self.output_labels = utils.load_array_if_path(output_labels)
         else:
